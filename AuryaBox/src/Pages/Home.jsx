5f6c1de90768f33652a0b9f75d12195b4f46e993
aurelien.mlk92 <aurelien.geninsimonet@gmail.com>
2023-11-23 19:19:14 +0100

    merge Ulysse

--- AuryaBox/src/Pages/Home.jsx
+++ AuryaBox/src/Pages/Home.jsx
@@ -1,46 +1,26 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-import Footer from "../Components/Footer";
-import NavBar from "../Components/NavBar";
-
-function Home () {
-    return (
-    <>   
-    <NavBar />
-    <main>
-        <p>HELLO</p>
-    </main>
-    <Footer />
-    </> 
-    )
-=======
-function Home() {
-  return (
-    <section className="styleHome">
-      <div className="inputListNoel">Offrir</div>
-      <div className="inputListNoel">Recevoir</div>
-    </section>
-  );
->>>>>>> 383b2567072f2695fa8adca9d47b5284354c66bd
-=======
 import { useNavigate } from "react-router-dom";
 import "./home.css";
+import NavBar from "../Components/NavBar";
+import Footer from "../Components/Footer";
 
 function Home() {
   const navigate = useNavigate();
   return (
+    <>
+    <NavBar />
     <section className="styleHome">
       <div className="itemColonne">
         <div className="inputListeNoel1 inputListNoel">
           <p className="Textecss">
-            Offrir : <br></br>
+            " Offrir : <br></br>
             <br></br>Remplir une boite type boite a chaussures ( 5 objets max
             1kilos par objets) le principe c'est qu'il faut que sa rentre
             dedans, le nombre les gouts et les couleurs sont a votre convenance
             l'important et d'offrir et donner un sourir ce qui n'a pas de prix.
             il faut que les produits sois en très bonne état ou neuf et
             fonctionnels, nous vous proposont ici quelques thème afin de vous
-            guidez dans votre don. vous pouvez mélangez biensur plusieurs thème.
+            guidez dans votre don. vous pouvez mélangez biensur plusieurs
+            thème."
           </p>
         </div>
         <button
@@ -54,7 +34,7 @@ function Home() {
       <div className="itemColonne">
         <div className="inputListeNoel2 inputListNoel">
           <p className="Textecss">
-            Recevoir : <br></br>
+            "Recevoir : <br></br>
             <br></br> C'est Noel laissez vous guidez par le hasard qui fera bien
             les choses, trouvez la boite qui vous plais cliquez dessus vous
             recevrez une notificartion pour la reception en point relais,
@@ -71,8 +51,9 @@ function Home() {
         </button>
       </div>
     </section>
+    <Footer />
+    </>
   );
->>>>>>> c8cb97067a00ef3c3ca7d086e0c92025552dfeb8
 }
 
 export default Home;
