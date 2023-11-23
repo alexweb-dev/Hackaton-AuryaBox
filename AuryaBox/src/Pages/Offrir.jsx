335a7f9b94e5ceb980ef4d42d96cd7bd6add5491
Alex Kandel <alex.kandel@wanadoo.fr>
2023-11-23 17:43:08 +0100

    box terminées

--- AuryaBox/src/Pages/Offrir.jsx
+++ AuryaBox/src/Pages/Offrir.jsx
@@ -1,5 +1,58 @@
-import "./Offrir.css"
-function Offrir () {
-    return (<div className="pageOffrirTitre"><h1>Offrir</h1></div>)
+import "./Offrir.css";
+function Offrir() {
+  return (
+    <div className="pageOffrir">
+      <div className="pageOffrirTitre">
+        <h1>Offrir</h1>
+      </div>
+      <div className="allBoxes">
+        <div className="themeUn theme">
+          <button>-</button>
+          <p>Jouet</p>
+          <button>+</button>
+        </div>
+        <div className="themeDeux theme">
+          <button>-</button>
+          <p>Multimedia</p>
+          <button>+</button>
+        </div>
+        <div className="themeTrois theme">
+          <button>-</button>
+          <p>Livre</p>
+          <button>+</button>
+        </div>
+        <div className="themeQuatre theme">
+          <button>-</button>
+          <p>Nourriture</p>
+          <button>+</button>
+        </div>
+        <div className="themeCinq theme">
+          <button>-</button>
+          <p>Voyage</p>
+          <button>+</button>
+        </div>
+        <div className="themeSix theme">
+          <button>-</button>
+          <p>Cosmétique</p>
+          <button>+</button>
+        </div>
+        <div className="themeSept theme">
+          <button>-</button>
+          <p>Sport</p>
+          <button>+</button>
+        </div>
+        <div className="themeHuit theme">
+          <button>-</button>
+          <p>Jeu de Société</p>
+          <button>+</button>
+        </div>
+        <div className="themeNeuf theme">
+          <button>-</button>
+          <p>Divers</p>
+          <button>+</button>
+        </div>
+      </div>
+    </div>
+  );
 }
-export default Offrir; 
+export default Offrir;
