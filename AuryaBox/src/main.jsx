af890aa37744f7958c4f2b58acf5a85bbb496a2a
FIRST_NAME LAST_NAMEUdtrx <dutrieux.ulysse@gmail.com>
2023-11-23 14:12:43 +0100

    create router in main

--- AuryaBox/src/main.jsx
+++ AuryaBox/src/main.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import Home from "./Pages/Home.jsx";
+import Donneur from "./Pages/Donneur.jsx";
+import Receveur from "./Pages/Receveur.jsx";
+import PopupDonneur from "./Pages/PopupDonneur.jsx";
+import PopupReceveur from "./Pages/PopupReceveur.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/Home",
+        element: <Home />,
+      },
+      {
+        path: "/Donneur",
+        element: <Donneur />,
+      },
+      {
+        path: "/Receveur",
+        element: <Receveur />,
+      },
+      {
+        path: "/PopupDonneur",
+        element: <PopupDonneur />,
+      },
+      {
+        path: "/PopupReceveur",
+        element: <PopupReceveur />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
