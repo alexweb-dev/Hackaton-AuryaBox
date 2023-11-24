4afd178715a77d362781316d25363a562c8b93cb
Alex Kandel <alex.kandel@wanadoo.fr>
2023-11-24 01:35:04 +0100

    compteur fonctionnel

--- AuryaBox/src/Pages/Offrir.jsx
+++ AuryaBox/src/Pages/Offrir.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import Jouet from "../Pictures/jouets.png";
 import Multimedia from "../Pictures/multimedia-interactif.png";
 import Livre from "../Pictures/livre.png";
@@ -6,93 +6,214 @@ import Nourriture from "../Pictures/nourriture.png";
 import Voyage from "../Pictures/voyage.png";
 import Cosmétique from "../Pictures/savon.png";
 import Sport from "../Pictures/sports.png";
-import Vetement from "../Pictures/t-shirt.png"
+import Vetement from "../Pictures/t-shirt.png";
 import Divers from "../Pictures/divers.png";
 import "./Offrir.css";
 
 function Offrir() {
   const [count, setCount] = useState(0);
   const [nbBoite, setNbBoite] = useState(0);
-  //   const [countJouet, setcountJouet] = useState(0);
-  //   const [countMultimédia, setcountMultimédia] = useState(0);
-  //   const [countLivre, setcountLivre] = useState(0);
-  //   const [countNourriture, setcountNourriture] = useState(0);
-  //   const [countVoyage, setcountVoyage] = useState(0);
-  //   const [countCosmétique, setcountCosmétique] = useState(0);
-  //   const [countSport, setcountSport] = useState(0);
-  //   const [countJeudDeSociété, setcountJeuDeSociété] = useState(0);
+  const prevCountRef = useRef();
+  const [countVetement, setCountVetement] = useState(0);
+  const [countJouet, setCountJouet] = useState(0);
+  const [countMultimédia, setCountMultimédia] = useState(0);
+  const [countLivre, setCountLivre] = useState(0);
+  const [countNourriture, setCountNourriture] = useState(0);
+  const [countVoyage, setCountVoyage] = useState(0);
+  const [countCosmétique, setCountCosmétique] = useState(0);
+  const [countSport, setCountSport] = useState(0);
+  const [countDivers, setcountDivers] = useState(0);
+  // const incrementCount = () => {
+  //   setCount(count + 1);
+  // };
 
-  const incrementCount = () => {
+  // const decrementCount = () => {
+  //   if (count > 0) {
+  //     setCount(count - 1);
+  //   }
+  // };
+
+  useEffect(() => {
+    const prevCount = prevCountRef.current;
+    prevCountRef.current = count;
+
+    if (count > 0) {
+      if (count % 5 === 4 && count < prevCount) {
+        setNbBoite(nbBoite - 1);
+      } else if (count % 5 === 0 && count > prevCount) {
+        setNbBoite(nbBoite + 1);
+      }
+    }
+  }, [count]);
+  console.log(count, nbBoite);
+
+  // Pour Vêtement
+  const incrementCountVetement = () => {
+    setCountVetement(countVetement + 1);
     setCount(count + 1);
   };
 
-  const decrementCount = () => {
-    if (count > 0) {
+  const decrementCountVetement = () => {
+    if (countVetement > 0) {
+      setCountVetement(countVetement - 1);
       setCount(count - 1);
     }
   };
 
-  console.log(count);
-  function boites() {
-    if (count % 5 === 0) {
-      setNbBoite(nbBoite + 1);
-    } else if (count % 5 === 4) {
-      setNbBoite(nbBoite - 1);
-    }
-  }
-  console.log(nbBoite);
+  // Pour Jouet
+  const incrementCountJouet = () => {
+    setCountJouet(countJouet + 1);
+    setCount(count + 1);
+  };
 
+  const decrementCountJouet = () => {
+    if (countJouet > 0) {
+      setCountJouet(countJouet - 1);
+      setCount(count - 1);
+    }
+  };
+
+  // Pour Multimédia
+  const incrementCountMultimedia = () => {
+    setCountMultimédia(countMultimédia + 1);
+    setCount(count + 1);
+  };
+
+  const decrementCountMultimedia = () => {
+    if (countMultimédia > 0) {
+      setCountMultimédia(countMultimédia - 1);
+      setCount(count - 1);
+    }
+  };
+
+  // Pour Livre
+  const incrementCountLivre = () => {
+    setCountLivre(countLivre + 1);
+    setCount(count + 1);
+  };
+
+  const decrementCountLivre = () => {
+    if (countLivre > 0) {
+      setCountLivre(countLivre - 1);
+      setCount(count - 1);
+    }
+  };
+
+  // Pour Nourriture
+  const incrementCountNourriture = () => {
+    setCountNourriture(countNourriture + 1);
+    setCount(count + 1);
+  };
+
+  const decrementCountNourriture = () => {
+    if (countNourriture > 0) {
+      setCountNourriture(countNourriture - 1);
+      setCount(count - 1);
+    }
+  };
+
+  // Pour Voyage
+  const incrementCountVoyage = () => {
+    setCountVoyage(countVoyage + 1);
+    setCount(count + 1);
+  };
+
+  const decrementCountVoyage = () => {
+    if (countVoyage > 0) {
+      setCountVoyage(countVoyage - 1);
+      setCount(count - 1);
+    }
+  };
+
+  // Pour Cosmétique
+  const incrementCountCosmétique = () => {
+    setCountCosmétique(countCosmétique + 1);
+    setCount(count + 1);
+  };
+
+  const decrementCountCosmétique = () => {
+    if (countCosmétique > 0) {
+      setCountCosmétique(countCosmétique - 1);
+      setCount(count - 1);
+    }
+  };
+
+  // Pour Sport
+  const incrementCountSport = () => {
+    setCountSport(countSport + 1);
+    setCount(count + 1);
+  };
+
+  const decrementCountSport = () => {
+    if (countSport > 0) {
+      setCountSport(countSport - 1);
+      setCount(count - 1);
+    }
+  };
+
+  // Pour Divers
+  const incrementCountDivers = () => {
+    setcountDivers(countDivers + 1);
+    setCount(count + 1);
+  };
+
+  const decrementCountDivers = () => {
+    if (countDivers > 0) {
+      setcountDivers(countDivers - 1);
+      setCount(count - 1);
+    }
+  };
+  console.log(countVetement,countJouet,countMultimédia,countLivre,countNourriture,countVoyage,countCosmétique,countSport,countDivers)
   return (
     <div className="pageOffrir">
       <div className="pageOffrirTitre">
         <h1>Offrir</h1>
-        <p>{boites}</p>
       </div>
       <div className="allBoxes">
         <div className="themeUn theme">
-          <button onClick={decrementCount}>-</button>
+          <button onClick={decrementCountVetement}>-</button>
           <img src={Vetement} alt="Vêtements" />
-          <button onClick={incrementCount}>+</button>
+          <button onClick={incrementCountVetement}>+</button>
         </div>
         <div className="themeDeux theme">
-          <button onClick={decrementCount}>-</button>
+          <button onClick={decrementCountJouet}>-</button>
           <img src={Jouet} alt="Jouet" />
-          <button onClick={incrementCount}>+</button>
+          <button onClick={incrementCountJouet}>+</button>
         </div>
         <div className="themeTrois theme">
-          <button onClick={decrementCount}>-</button>
+          <button onClick={decrementCountMultimedia}>-</button>
           <img src={Multimedia} alt="Multimedia" />
-          <button onClick={incrementCount}>+</button>
+          <button onClick={incrementCountMultimedia}>+</button>
         </div>
         <div className="themeQuatre theme">
-          <button onClick={decrementCount}>-</button>
+          <button onClick={decrementCountLivre}>-</button>
           <img src={Livre} alt="Livre" />
-          <button onClick={incrementCount}>+</button>
+          <button onClick={incrementCountLivre}>+</button>
         </div>
         <div className="themeCinq theme">
-          <button onClick={decrementCount}>-</button>
+          <button onClick={decrementCountNourriture}>-</button>
           <img src={Nourriture} alt="Nourriture" />
-          <button onClick={incrementCount}>+</button>
+          <button onClick={incrementCountNourriture}>+</button>
         </div>
         <div className="themeSix theme">
-          <button onClick={decrementCount}>-</button>
+          <button onClick={decrementCountVoyage}>-</button>
           <img src={Voyage} alt="Voyage" />
-          <button onClick={incrementCount}>+</button>
+          <button onClick={incrementCountVoyage}>+</button>
         </div>
         <div className="themeSept theme">
-          <button onClick={decrementCount}>-</button>
+          <button onClick={decrementCountCosmétique}>-</button>
           <img src={Cosmétique} alt="Cosmétique" />
-          <button onClick={incrementCount}>+</button>
+          <button onClick={incrementCountCosmétique}>+</button>
         </div>
         <div className="themeHuit theme">
-          <button onClick={decrementCount}>-</button>
+          <button onClick={decrementCountSport}>-</button>
           <img src={Sport} alt="Sport" />
-          <button onClick={incrementCount}>+</button>
+          <button onClick={incrementCountSport}>+</button>
         </div>
         <div className="themeNeuf theme">
-          <button onClick={decrementCount}>-</button>
+          <button onClick={decrementCountDivers}>-</button>
           <img src={Divers} alt="Divers" />
-          <button onClick={incrementCount}>+</button>
+          <button onClick={incrementCountDivers}>+</button>
         </div>
       </div>
     </div>
