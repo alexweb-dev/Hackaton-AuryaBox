59126411a71038b1d05200cafa54e0ee054a08bc
FIRST_NAME LAST_NAMEUdtrx <dutrieux.ulysse@gmail.com>
2023-11-24 03:20:54 +0100

    before merge adjust and pull AlexAmazingWork

--- AuryaBox/src/Components/NavBar.jsx
+++ AuryaBox/src/Components/NavBar.jsx
@@ -1,17 +1,24 @@
-import '../Components/navbar.css'
+import { useNavigate } from "react-router-dom";
+import "../Components/navbar.css";
 
 function NavBar() {
+  const navigate = useNavigate();
   return (
-    <nav className='first'>
-      <img id="logo" src="src\Pictures\AUrya (2).png" alt="" />
-      <div id='navbar'>
-      <h3>ACCEUIL</h3>
-      <h3>OFFRIR</h3>
-      <h3>RECEVOIR</h3>
-      <div className='button'>
-      <button id='button1'>LOGIN</button>
-      <button id='button2'>SIGN-UP</button>
-      </div>
+    <nav className="first">
+      <img
+        id="logo"
+        src="src\Pictures\AUrya (2).png"
+        alt=""
+        onClick={() => navigate("/")}
+      />
+      <div id="navbar">
+        <h3 onClick={() => navigate("/")}>ACCEUIL</h3>
+        <h3 onClick={() => navigate("/Offrir")}>AU PIED DU SAPIN</h3>
+        <h3 onClick={() => navigate("/Receveur")}>LA FABNRIQUE</h3>
+        <div className="button">
+          <button id="button1">LOGIN</button>
+          <button id="button2">SIGN-UP</button>
+        </div>
       </div>
     </nav>
   );
--- AuryaBox/src/Pages/Home.jsx
+++ AuryaBox/src/Pages/Home.jsx
@@ -10,7 +10,28 @@ function Home() {
       <NavBar />
       <section className="styleHome">
         <div className="itemColonne">
-          <div className="inputListeNoel1 inputListNoel">
+          <div className="inputListeNoel2">
+            <p className="Textecss">
+              Recevoir : <br></br>
+              <br></br> C'est Noel laissez vous guidez par le hasard qui fera
+              bien les choses, trouvez la boite qui vous plais cliquez dessus
+              vous recevrez une notificartion pour la reception en point relais,
+              click&collect, ou même dans votre association préferez retirez
+              votre boite magic et profitez-en sans modération.
+            </p>
+          </div>
+          <div id="bouttonLaFabrique">
+            <button
+              className="bouttonHome"
+              type="button"
+              onClick={() => navigate("/Receveur")}
+            >
+              Au pied du sapin
+            </button>
+          </div>
+        </div>
+        <div className="itemColonne">
+          <div className="inputListeNoel1">
             <p className="Textecss">
               Offrir : <br></br>
               <br></br>Remplir une boite type boite a chaussures 5 objets max
@@ -24,27 +45,6 @@ function Home() {
             </p>
           </div>
           <div id="bouttonLesCadeaux">
-            <button
-              className="bouttonHome"
-              type="button"
-              onClick={() => navigate("/Receveur")}
-            >
-              Les cadeaux
-            </button>
-          </div>
-        </div>
-        <div className="itemColonne">
-          <div className="inputListeNoel2 inputListNoel">
-            <p className="Textecss">
-              Recevoir : <br></br>
-              <br></br> C'est Noel laissez vous guidez par le hasard qui fera
-              bien les choses, trouvez la boite qui vous plais cliquez dessus
-              vous recevrez une notificartion pour la reception en point relais,
-              click&collect, ou même dans votre association préferez retirez
-              votre boite magic et profitez-en sans modération.
-            </p>
-          </div>
-          <div id="bouttonLaFabrique">
             <button
               className="bouttonHome"
               type="button"
